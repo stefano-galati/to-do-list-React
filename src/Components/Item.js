0cfcefb96e016f693af4947170d0ff5cab1a9675
stefano-galati <stefanogalati2002@gmail.com>
2024-03-28 21:40:16 +0100

    Created Container and Item

---
+++ src/Components/Item.js
@@ -0,0 +1,9 @@
+export default function Item(props){
+    return(
+        <div className="item">
+            <p>{props.name}</p>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
